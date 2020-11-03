32830b2826a7ac7153389c8c98e1cbabe54643af
christianJR98 <cristian-1823@live.com.mx>
2020-11-03 12:45:11 -0600

    generate json web token

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ import {notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express()
+
+app.use(express.json())
 
 dotenv.config()
 
@@ -15,12 +18,12 @@ connectDB()
 app.get('/',(req,res)=>{
     res.send('API is running')
 })
-
+ 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //Error de ruta invalida
 app.use(notFound)
-
 
 app.use(errorHandler)
 
