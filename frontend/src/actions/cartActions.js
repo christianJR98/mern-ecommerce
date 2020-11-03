ca7ca192f01ca3c5ab64db794e84223ebe6ec1b9
christianJR98 <cristian-1823@live.com.mx>
2020-11-03 00:18:53 -0600

    implement remove items from cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM,CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 //Get state nos permite acceder a cualquier parte del store
 export const addToCart = (id, qty) => async (dispatch, getState) =>{
@@ -21,5 +21,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) =>{
     localStorage.setItem('cartItems', JSON.stringify( getState().cart.cartItems))
 }
 
+export const removeFromCart = (id) => (dispatch, getState) =>{
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id
+    })
 
+    localStorage.getItem('cartItems',JSON.stringify(getState().cart.cartItems))
+}
 
