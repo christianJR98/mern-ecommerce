fb2ca00d08090c7071719e5e4321be13d24aebe7
christianJR98 <cristian-1823@live.com.mx>
2020-11-08 16:52:48 -0600

    show orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,8 +80,20 @@ const updateOrderToPaid = asyncHandler( async(req,res) => {
     }
 })
 
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private 
+const getMyOrders = asyncHandler( async(req,res) => {
+    //Se agrega el name y Email de user
+    const orders = await Order.find({user: req.user._id})
+
+    res.json(orders)
+
+})
+
 export {
     addOrderItems,
     getOrderById,
-    updateOrderToPaid
+    updateOrderToPaid,
+    getMyOrders
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,7 +13,8 @@ import {
 import { 
     orderCreateReducer,
     orderDetailsReducer,
-    orderPayReducer
+    orderPayReducer,
+    orderListMyReducer
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -26,7 +27,8 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
     orderDetails:orderDetailsReducer,
-    orderPay: orderPayReducer
+    orderPay: orderPayReducer,
+    orderListMy : orderListMyReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) 
