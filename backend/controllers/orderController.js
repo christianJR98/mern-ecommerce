4074526ebbb571048eb688dbd8f0752b556a6d99
christianJR98 <cristian-1823@live.com.mx>
2020-11-07 19:30:17 -0600

    get order by id endpoint and order details reducer & action

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,6 +37,24 @@ const addOrderItems = asyncHandler( async(req,res) => {
     }
 })
 
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private 
+const getOrderById = asyncHandler( async(req,res) => {
+    //Se agrega el name y Email de user
+    const order = await Order.findById(req.params.id).populated(
+        'user', 
+        'name email'
+    )
+    if(order){
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
 export {
-    addOrderItems
+    addOrderItems,
+    getOrderById
 }
